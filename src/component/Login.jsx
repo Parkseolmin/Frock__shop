3651d52db7516df1f74348500b067ac3e829078c
Parkseolmin <qkrtjfals2@naver.com>
2023-08-11 00:43:44 +0900

    commit-message

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -22,36 +22,38 @@ export default function Login({ setAuthenticate }) {
   return (
     <div>
       <Container>
-        <div className='login__background'>
-          <div className='login__input'>
-            <Form onSubmit={(event) => loginUser(event)}>
-              <Form.Group className='mb-3' controlId='formBasicEmail'>
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                  type='email'
-                  placeholder='Enter email'
-                  onChange={(event) => setId(event.target.value)}
-                />
-                <Form.Text className='text-muted'>
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
+        <div className='loginContainer'>
+          <div className='login__background'>
+            <div className='login__input'>
+              <Form onSubmit={(event) => loginUser(event)}>
+                <Form.Group className='mb-3' controlId='formBasicEmail'>
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    type='email'
+                    placeholder='Enter email'
+                    onChange={(event) => setId(event.target.value)}
+                  />
+                  <Form.Text className='text-muted'>
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
 
-              <Form.Group className='mb-3' controlId='formBasicPassword'>
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type='password'
-                  placeholder='Password'
-                  onChange={(event) => setPassword(event.target.value)}
-                />
-              </Form.Group>
-              <Form.Group className='mb-3' controlId='formBasicCheckbox'>
-                <Form.Check type='checkbox' label='Check me out' />
-              </Form.Group>
-              <Button variant='secondary' type='submit'>
-                Login
-              </Button>
-            </Form>
+                <Form.Group className='mb-3' controlId='formBasicPassword'>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type='password'
+                    placeholder='Password'
+                    onChange={(event) => setPassword(event.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='formBasicCheckbox'>
+                  <Form.Check type='checkbox' label='Check me out' />
+                </Form.Group>
+                <Button variant='secondary' type='submit'>
+                  Login
+                </Button>
+              </Form>
+            </div>
           </div>
         </div>
       </Container>
